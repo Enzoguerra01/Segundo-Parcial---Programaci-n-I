ce4a4326f969f92a08cc499cb6018513c81fecde
Enzoguerra01 <56485765+Enzoguerra01@users.noreply.github.com>
2022-07-08 01:17:57 -0700

    Agrego las rutas necesarias para todo el proyecto

--- public/rutas.php
+++ public/rutas.php
@@ -4,6 +4,11 @@
     Routes::AddView("/","blog");
     Routes::AddView("/login","login");
     Routes::Add("/login","post","SesionControlador::IniciarSesion");
+    Routes::Add("/cerrar_sesion","get","SesionControlador::CerrarSesion");
+    Routes::AddView("/usuarios/alta","register");
+    Routes::Add("/usuarios/alta","post","UsuarioControlador::Alta");
+    Routes::AddView("/crear_publicacion","crear_publicacion");
+    Routes::Add("/crear_publicacion","post","PublicacionControlador::CrearPublicacion");
     
     Routes::Run();
 
